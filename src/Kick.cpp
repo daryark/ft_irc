7ccaf34aae0fb96524d2e749138efc04df61a874
Dasha Yarkovska <mperetia@student.42wolfsburg.de>
2025-12-05 20:17:09 +0100

    save

--- src/Kick.cpp
+++ src/Kick.cpp
@@ -2,10 +2,7 @@
 
 void Command::executeKick(Client *client)
 {
-    if(_args.size() != 4)
-        return client->queueMsg(ERR_NEEDMOREPARAMS(client->getNickname(), "KICK"));
-
-    if (!checkPreconditions(client, 2))
+    if (!checkPreconditions(client, 4))
         return ;
 
     //_args[0]- it depends of place where sen the command 
@@ -13,11 +10,12 @@ void Command::executeKick(Client *client)
     // _args[0] = #target_channel_name 
     const std::vector<std::string> channels = splitVec(_args[1], ',');
     const std::vector<std::string> nicks = splitVec(_args[3], ',');
-    const std::string comment = _args.size() > 2 ? joinVecIntoStr(_args.begin() + 2, _args.end()) : "is kicked";
+    const std::string comment = _args.size() > 3 ? joinVecIntoStr(_args.begin() + 3, _args.end()) : "is kicked";
     
 
-    std::cout << "chanel = " << channels[0] << std::endl;
-    std::cout << "nicks = " << nicks[0] << std::endl;
+    // std::cout << "chanel = " << channels[0] << std::endl;
+    // std::cout << "nicks = " << nicks[0] << std::endl;
+    // std::cout << "comment = " << comment;
 
     if (channels.size() == 1)
     {
@@ -47,8 +45,7 @@ void Command::executeKickOne(Client* client, const std::string& channel_name, co
     else if(!channel->isMember(target_client))
         return client->queueMsg(ERR_USERNOTINCHANNEL(target_nick, channel_name));
     
-    channel->globalMessage(client, 
-    MSG(client->getNickname(), client->getUsername(), client->getHostname(), _command, channel_name, comment), true);
+    channel->globalMessage(client, MSG(client->getNickname(), client->getUsername(), client->getHostname(), _command, channel_name, comment), true);
 
     channel->removeClient(target_client);
     target_client->removeFromChannel(channel_name);
@@ -56,6 +53,7 @@ void Command::executeKickOne(Client* client, const std::string& channel_name, co
     if (channel->getSize() == 0){
         // _server->deleteChannel(target_nick); //????
         _server->deleteChannel(channel_name);
+        return ;
     }
 
     channel->globalMessage(client, RPL_NAMREPLY(client->getNickname(), channel->getName(), formChannelMembersList(channel)), true);
